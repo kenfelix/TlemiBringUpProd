ee0e4e55829abf771062dc9380128cc6a9b4b8c2
kenfelix <Kmrapper.kf@gmail.com>
2022-08-02 00:33:29 +0100

    error fix

--- backend/migrate.sh
+++ backend/migrate.sh
@@ -4,4 +4,4 @@ DJANGO_SUPER_EMAIL=${DJANGO_SUPER_EMAIL:-"emekaokafor08056@gmail.com"}
 cd /app/
 
 /opt/venv/bin/python manage.py migrate --noinput
-/opt/venv/bin/python manage.py createsuperuser --email ${DJANGO_SUPER_EMAIL} --noinput || true
+/opt/venv/bin/python manage.py createsuperuser --email $DJANGO_SUPER_EMAIL --noinput || true
