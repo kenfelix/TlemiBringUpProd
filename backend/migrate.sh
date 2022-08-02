f1e710985d1822f6a46f8fcdf7200fa9afd02909
kenfelix <Kmrapper.kf@gmail.com>
2022-08-02 11:18:52 +0100

    updates

--- backend/migrate.sh
+++ backend/migrate.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-DJANGO_SUPER_EMAIL=${DJANGO_SUPER_EMAIL:-"emekaokafor08056@gmail.com"}
+SUPER_EMAIL=${DJANGO_SUPER_EMAIL:-"emekaokafor08056@gmail.com"}
 cd /app/
 
 /opt/venv/bin/python manage.py migrate --noinput
-/opt/venv/bin/python manage.py createsuperuser --email $DJANGO_SUPER_EMAIL --noinput || true
+/opt/venv/bin/python manage.py createsuperuser --email $SUPER_EMAIL --noinput || true
